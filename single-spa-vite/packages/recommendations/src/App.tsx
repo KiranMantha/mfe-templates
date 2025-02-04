87c66c951f95f55077f1b1912d0be95c141e80ea
KiranMantha <kiranv.mantha@gmail.com>
2025-02-04 09:52:51 +0530

    refactored remote apps to use custom hooks from utils

--- single-spa-vite/packages/recommendations/src/App.tsx
+++ single-spa-vite/packages/recommendations/src/App.tsx
@@ -1,33 +1,12 @@
-import { useEffect } from 'react';
 import { BrowserRouter, Link, MemoryRouter, Route, Routes } from 'react-router-dom';
+import { useSPARouting } from 'utils';
 import './App.css';
-
-const fromEvent = (target: Window, eventName: string, onNext: EventListener, options = false) => {
-  target.addEventListener(eventName, onNext, options);
-  const unsubscribe = () => {
-    target.removeEventListener(eventName, onNext, options);
-  };
-  return unsubscribe;
-};
 
 function App({ isSingleSpa, basename = '/' }: { isSingleSpa: boolean; basename: string }) {
   const Router = isSingleSpa ? MemoryRouter : BrowserRouter;
-  // Sync with host router
-  useEffect(() => {
-    if (isSingleSpa) {
-      const hostNavigate = (event: Event) => {
-        console.log('single-spa:routing-event', event);
-        const customEvent = event as CustomEvent;
-        const newUrl: string = customEvent.detail?.newUrl || '/';
-        const newPath = new URL(newUrl, window.location.origin).pathname; // Extract pathname
 
-        window.history.pushState(null, '', newPath);
-      };
+  useSPARouting(isSingleSpa, basename);
 
-      const unsubscribe = fromEvent(window, 'single-spa:routing-event', hostNavigate);
-      return () => unsubscribe();
-    }
-  }, [basename, isSingleSpa]);
   return (
     <Router basename={basename}>
       <div className="remote-app">
