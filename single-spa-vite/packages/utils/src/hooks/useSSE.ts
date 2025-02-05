6dab1fc458be75e3fb6543eacd92bd953a3e5c13
KiranMantha <kiranv.mantha@gmail.com>
2025-02-05 11:06:24 +0530

    updated useSSE hook

--- single-spa-vite/packages/utils/src/hooks/useSSE.ts
+++ single-spa-vite/packages/utils/src/hooks/useSSE.ts
@@ -1,5 +1,5 @@
 import { fetchEventSource } from '@microsoft/fetch-event-source';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { emitCustomEvent } from '../helpers';
 import { useSignal } from './useSignal';
 
@@ -9,10 +9,12 @@ const REACT_APP_SSE_URL = 'http://localhost:3000/sse';
 
 export const useSSE = () => {
   const controller = useSignal<AbortController>();
+  const message = useRef('');
 
   useEffect(() => {
     const newController = new AbortController();
     controller.set(newController);
+    message.current = '';
     fetchEventSource(REACT_APP_SSE_URL, {
       headers: {},
       signal: newController.signal,
@@ -22,7 +24,8 @@ export const useSSE = () => {
       },
       onmessage: (e) => {
         console.log('sseMessage', e.data);
-        if (e.data) {
+        if (e.data && e.data != message.current) {
+          message.current = e.data;
           const sseMessage = JSON.parse(e.data) as SSEMessage;
           emitCustomEvent(document, 'sse', sseMessage);
         }
